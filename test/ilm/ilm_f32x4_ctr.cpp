739a67e5377668945880ac16ee3a19606d473b87
Christophe Riccio <mail@g-truc.net>
2013-03-11 22:02:58 +0100

    Added == and != on f32x4 tests

--- test/ilm/ilm_f32x4_ctr.cpp
+++ test/ilm/ilm_f32x4_ctr.cpp
@@ -18,20 +18,30 @@ namespace equal_op
 	{
 		ilm::f32x4 input0;
 		ilm::f32x4 input1;
-		ilm::f32x4 output0;
-		bool result
+		bool output0;
 	};
 
 	static entry Set[] = 
 	{
-		{ilm::f32x4(0.0f), ilm::f32x4(0.0f, 0.0f, 0.0f, 0.0f), },
+		{ilm::f32x4(), ilm::f32x4(0.0f), true},
+		{ilm::f32x4(0.0f), ilm::f32x4(0.0f, 0.0f, 0.0f, 0.0f), true},
+		{ilm::f32x4(1.0f), ilm::f32x4(1.0f, 1.0f, 1.0f, 1.0f), true},
+		{ilm::f32x4(1.0f, 2.0f, 3.0f, 4.0f), ilm::f32x4(1.0f, 2.0f, 3.0f, 4.0f), true},
+		{ilm::f32x4(1.0f, 2.0f, 3.0f, 4.0f), ilm::f32x4(1.0f, 2.0f, 3.0f, 8.0f), false},
+		{ilm::f32x4(1.0f, 2.0f, 3.0f, 4.0f), ilm::f32x4(1.0f, 2.5f, 3.0f, 4.0f), false},
+		{ilm::f32x4(1.0f, 2.0f, 3.0f, 4.0f), ilm::f32x4(-1.0f, 2.5f, 3.0f, 4.0f), false},
+		{ilm::f32x4(1.5f, 2.3f, 3.2f, 4.9f), ilm::f32x4(1.1f, 2.5f, 3.8f, 4.1f), false}
 	};
 
 	int test()
 	{
 		int Error(0);
 
-		
+		for(std::size_t i = 0; i < sizeof(Set) /  sizeof(entry); ++i)
+		{
+			bool Result = Set[i].input0 == Set[i].input1;
+			Error += Result == Set[i].output0 ? 0 : 1;
+		}
 
 		return Error;
 	}
